0b77d3df1ae127aa6e506b4a3a461f5239bf117f
skywie <festinelijah@gmail.com>
2026-07-29 20:16:26 +0800

    feat: add 404 not found page with catch-all route + error boundary to prevent showing white blank screen when there's an error

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return(
-    <div className="bg-[#232323] h-80 w-full mt-20">
+    <div className="bg-[#232323] mt-auto h-50 w-full">
       <p className="text-white">Test</p>
     </div>
   )
