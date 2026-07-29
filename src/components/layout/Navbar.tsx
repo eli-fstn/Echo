a6f54a5147234a6e19913fef824e2c9b856f00e6
skywie <festinelijah@gmail.com>
2026-07-29 14:35:06 +0800

    feat(report): added partial design of summary report of the GWA

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,7 +1,12 @@
+import logo from "../../assets/Echo-Logo.png";
+
 function Navbar() {
   return (
     <div className="bg-white flex shadow-sm flex-row justify-between items-center px-90 h-15">
-      <p className="font-mono text-[#232323] text-[1.7rem] leading-none">Echo</p>
+      <div className="flex flex-row items-center">
+        <img src={logo} className="w-10 h-10 rounded mr-1" />
+        <p className="font-mono text-[#232323] text-[1.7rem] mt-1">Echo</p>
+      </div>
       <p className="font-inter font-semibold text-[#232323] text-[1.2rem]">GWA Calculator</p>
     </div>
   )
