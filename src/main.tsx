5c63821d6eb41d21681f8c26b85f18c940631270
skywie <festinelijah@gmail.com>
2026-07-31 21:29:05 +0800

    fix: changed the vercel analytics & speed insights into react version

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from '@vercel/analytics/react'
 import './style/style.css'
 import App from './App.tsx'
 
